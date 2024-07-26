0391c6ee4059a0cfc4de9eeb8092e0878abf336a
AnaliApps <ronoanali5@gmail.com>
2024-07-26 17:49:51 +0300

    Added styling to footer
    - Added styling to footer to fix it to the bottom of the page always

--- src/footer.js
+++ src/footer.js
@@ -2,26 +2,17 @@ export default function Footer(){
     let div = document.createElement('div');
     div.setAttribute('class','footer')
     let links= document.createElement('ul')
-    let li_facebook = document.createElement('li')
-    let li_facebook_a = document.createElement('a')
-    li_facebook_a.innerHTML = 'Facebook'
-    li_facebook.appendChild(li_facebook_a)
-    let li_x = document.createElement('li')
-    let li_x_a = document.createElement('a')
-    li_x_a.innerHTML = 'X'
-    li_x.appendChild(li_x_a)
-    let li_instagram = document.createElement('li')
-    let li_instagram_a = document.createElement('a')
-    li_instagram_a.innerHTML = 'Instagram'
-    li_instagram.appendChild(li_instagram_a)
-    let li_youtube = document.createElement('li')
-    let li_youtube_a = document.createElement('a')
-    li_youtube_a.innerHTML = 'Youtube'
-    li_youtube.appendChild(li_youtube_a)
-    links.appendChild(li_facebook)
-    links.appendChild(li_instagram)
-    links.appendChild(li_x)
-    links.appendChild(li_youtube)
+    let li_github = document.createElement('li')
+    let li_github_a = document.createElement('a')
+    li_github_a.innerHTML = 'Github'
+    li_github.appendChild(li_github_a)
+    let li_copyright = document.createElement('li')
+    let li_copyright_a = document.createElement('a')
+    let date_ui = new Date().getFullYear();
+    li_copyright_a.innerHTML = `CopyRight&copy;${date_ui}`
+    li_copyright.appendChild(li_copyright_a)
+    links.appendChild(li_github)
+    links.appendChild(li_copyright)
     div.appendChild(links)
     return div
 }
